1ec59115ac30aca2d10a576544d8c910e459788e
mshahnaza <mamirovasahnaza042@gmail.com>
2025-07-09 13:13:33 +0600

    fibonacci tests implemented

---
+++ src/test/java/org/example/FibonacciTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FibonacciTest {
+
+    @Test
+    void shouldReturnSameFibonacci() {
+        for (int i = 0; i < 35; i++) {
+            long recursive = FibonacciAlgorithms.fibonacciRecursive(35);
+            long memoized = FibonacciAlgorithms.fibonacciMemoized(35);
+            long iterative = FibonacciAlgorithms.fibonacciIterative(35);
+
+            assertEquals(recursive, memoized);
+            assertEquals(recursive, iterative);
+            assertEquals(memoized, iterative);
+        }
+    }
+}
