34604c8f580b13de659e3863b9340fa72e52719b
mshahnaza <mamirovasahnaza042@gmail.com>
2025-07-11 01:04:21 +0600

    ParametrizedTest implemented

---
+++ src/main/java/org/example/test_runner/annotations/ParametrizedTest.java
@@ -0,0 +1,10 @@
+package org.example.test_runner.annotations;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ParametrizedTest {
+    int[] intValues() default {};
+    String[] stringValues() default {};
+}
