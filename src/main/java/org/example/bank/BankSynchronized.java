480db28470960e82600c570cd35debf402703d51
mshahnaza <mamirovasahnaza042@gmail.com>
2025-10-17 21:55:16 +0600

    banking simulator added

---
+++ src/main/java/org/example/bank/BankSynchronized.java
@@ -0,0 +1,13 @@
+package org.example.bank;
+
+public class BankSynchronized extends Bank{
+    public BankSynchronized(int numberOfAccounts, long minBalance, long maxBalance) {
+        super(numberOfAccounts, minBalance, maxBalance);
+    }
+
+    public synchronized void transfer(int from, int to, long amount) {
+        if (from == to || balances[from] < amount) return;
+        setAccountBalance(from, getAccountBalance(from) - amount);
+        setAccountBalance(to, getAccountBalance(to) + amount);
+    }
+}
